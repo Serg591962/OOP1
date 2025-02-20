806119ee5609c884419f46bf8333d642c9211cb5
Serg <swik591962@gmail.com>
2025-02-20 14:58:49 +0200

    возврат множественного значения

--- 1.py
+++ 1.py
@@ -1,6 +1,7 @@
-class Car:
-    @staticmethod # объявление статичного метода
-    def get_class_details():
-        print ("Это класс Car")
-Car.get_class_details()
-
+class Square:
+ 
+    @staticmethod
+    def get_squares(a, b):
+        return a*a, b*b
+ 
+print(Square.get_squares(3, 5))
