a2481ba81e2fba371929979502c1b42bae483efc
Serg <swik591962@gmail.com>
2025-02-20 12:49:26 +0200

    Обьявление класса с атрибутами и методами

--- 1.py
+++ 1.py
@@ -1,3 +1,14 @@
+# Создаем класс Car
 class Car:
-    pass
-car_a = Car()
+ 
+    # создаем атрибуты класса
+    name = "c200"
+    make = "mercedez"
+    model = 2008
+ 
+    # создаем методы класса
+    def start(self):
+        print ("Заводим двигатель")
+ 
+    def stop(self):
+        print ("Отключаем двигатель")
