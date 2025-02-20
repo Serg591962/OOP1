acf82a603582381eff5e16b8a7c6b67417319ccc
Serg <swik591962@gmail.com>
2025-02-20 14:37:04 +0200

    обьявление статичного метода

--- 1.py
+++ 1.py
@@ -1,17 +1,6 @@
-# Создаем класс Car
 class Car:
- 
-    # создаем атрибуты класса
-    name = "c200"
-    make = "mercedez"
-    model = 2008
- 
-    # создаем методы класса
-    def start(self):
-        print ("Заводим двигатель")
- 
-    def stop(self):
-        print ("Отключаем двигатель")
-car_b = Car()
-car_b.start()
-print(car_b.model)
+    @staticmethod # объявление статичного метода
+    def get_class_details():
+        print ("Это класс Car")
+Car.get_class_details()
+
